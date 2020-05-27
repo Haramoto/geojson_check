2252a31df0b38081bf114c61001c1bbbe7b585bd
Haramoto <haramoto_yoshiaki@yahoo.co.jp>
2020-05-27 12:27:08 +0900

    初回コミット

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Map msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+import Map from "./components/Map.vue";
 
 @Component({
   components: {
-    HelloWorld,
+    Map,
   },
 })
 export default class App extends Vue {}
---
+++ src/components/Map.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import Map from "ol/Map";
+import View from "ol/View";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+
+import "ol/ol.css";
+
+@Component
+export default class MapTag extends Vue {
+  mounted() {
+    let map = new Map({
+      view: new View({
+        center: [0, 0],
+        zoom: 1,
+      }),
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      target: "map",
+    });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+#map {
+  width: 600px;
+  height: 600px;
+}
+</style>
